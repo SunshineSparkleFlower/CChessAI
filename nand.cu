250809403f54143292e2ca873dc19548998cc171
andreas <andreashi2@gmail.com>
2014-12-02 13:37:38 +0100

    bug fixes i kernel

--- nand.cu
+++ nand.cu
@@ -9,12 +9,12 @@ __device__ int result;
 
 __device__ int cu_nand(piece_t *data, int *brain, int size, int idx) {
     if (data[idx] & brain[idx])
-        data[size] = 0;
+        data[size-1] = 0;
 }
 
 __global__ void cu_eval_curcuit(int* brainpart_value, int *M, int nr_ports,
         int *board, int board_size) {
-    const int data_size = 128 / 4 + 128 / 32 + 1;
+    const int data_size = 128 / 2 + 128 / 32 + 1;
     __shared__ piece_t data[data_size];
    
     if(threadIdx.x < 128)
@@ -26,7 +26,7 @@ __global__ void cu_eval_curcuit(int* brainpart_value, int *M, int nr_ports,
 
     int brain_idx = blockIdx.x;
     int i;
-    int size_port = nr_ports / 32 + 128 / 4;
+    int size_port = nr_ports / 32 + 128 / 2;
     int brain = brain_idx * (nr_ports * size_port);
 
 
@@ -36,11 +36,11 @@ __global__ void cu_eval_curcuit(int* brainpart_value, int *M, int nr_ports,
         cu_nand(data, M + brain + i, data_size, threadIdx.x);
         __syncthreads();
         if (threadIdx.x == 1 && blockIdx.x == 1 && data[data_size - 1])
-            SetBit(&data[128 / 4], i);
+            SetBit(&data[128 / 2], i);
 
 
     }
-    brainpart_value[brain_idx] = !!TestBit(&data[128 / 4], (nr_ports - 1));
+    brainpart_value[brain_idx] = !!TestBit(&data[128 / 2], (nr_ports - 1));
 
 }
 
@@ -57,7 +57,7 @@ int cu_score(AI_instance_t *ai, board_t *board) {
 
     cudaMemcpy(board->cu_board, board->board, sizeof (piece_t) * 128,
             cudaMemcpyHostToDevice);
-    cu_eval_curcuit << <ai->nr_ports / 32 + 128 / 4, ai->nr_brain_parts, 0, ai->stream>>>
+    cu_eval_curcuit << <ai->nr_ports / 32 + 128 / 2, ai->nr_brain_parts, 0, ai->stream>>>
             (cu_brainpart_value, ai->cu_brain, ai->nr_ports,
             (int*) board->cu_board, ai->board_size);
 
