2480f506abdc1db82d42622c2a59a5fa35604394
andreas <andreashi2@gmail.com>
2014-12-02 12:12:31 +0100

    kernal optimized futher

--- nand.cu
+++ nand.cu
@@ -7,60 +7,49 @@
 #include "nand.h"
 __device__ int result;
 
-__device__ int cu_nand(int *a, int *b, int size, piece_t *board, int board_size)
-{
-    int i;
-    int ret = 1;
-    for (i = 0; i < size / 32; i++) {
-        if (a[i] & b[i]) {
-            ret = 0;
-        }
-    }
-
-    for (i = 0; i < board_size / 32; i++) {
-        if (board[i] & b[i + size / 32]) {
-            ret = 0;
-        }
-    }
-    return ret;
-
+__device__ int cu_nand(piece_t *data, int *brain, int size, int idx) {
+    if (data[idx] & brain[idx])
+        data[size] = 0;
 }
 
 __global__ void cu_eval_curcuit(int* brainpart_value, int *M, int nr_ports,
-        piece_t *board, int board_size) {
-    __shared__ piece_t s_bord[128];
-    if (threadIdx.x == 1 && blockIdx.x == 1) {
-        for (int i = 0; i < 128; i++) {
-            s_bord[i] = board[i];
-        }
-    }
+        int *board, int board_size) {
+    const int data_size = 128 / 4 + 128 / 32 + 1;
+    __shared__ piece_t data[data_size];
+   
+    if(threadIdx.x < 128)
+        data[threadIdx.x] = board[threadIdx.x];
+    else
+        data[threadIdx.x-128] = 0;
+        
     __syncthreads();
-    const int max_prts = 128;
-    int V[max_prts/10];
 
     int brain_idx = blockIdx.x;
     int i;
-    int size_port = (nr_ports + board_size) / 32;
+    int size_port = nr_ports / 32 + 128 / 4;
     int brain = brain_idx * (nr_ports * size_port);
 
 
     for (i = 0; i < nr_ports; i++) {
         //get the value of port i using a offset
-        if (cu_nand(V, M + brain + i * size_port, nr_ports, s_bord, board_size))
-            SetBit(V, i);
+        data[data_size - 1] = 1;
+        cu_nand(data, M + brain + i, data_size, threadIdx.x);
+        __syncthreads();
+        if (threadIdx.x == 1 && blockIdx.x == 1 && data[data_size - 1])
+            SetBit(&data[128 / 4], i);
+
+
     }
-    brainpart_value[brain_idx] = !!TestBit(V, (nr_ports - 1));
+    brainpart_value[brain_idx] = !!TestBit(&data[128 / 4], (nr_ports - 1));
 
 }
 
-int cu_score(AI_instance_t *ai, board_t *board)
-{
-    dim3 blocks(ai->nr_brain_parts, 1);
-    dim3 grids(1, 1);
+int cu_score(AI_instance_t *ai, board_t *board) {
+
     //piece_t *cu_board;
     int *cu_brainpart_value;
     int *brainpart_value;
-
+    
     //allocate memory for CUDA
     //cudaMalloc(&cu_board, sizeof(piece_t)*64);
     cudaMalloc(&cu_brainpart_value, ai->nr_brain_parts * sizeof (int));
@@ -68,9 +57,9 @@ int cu_score(AI_instance_t *ai, board_t *board)
 
     cudaMemcpy(board->cu_board, board->board, sizeof (piece_t) * 128,
             cudaMemcpyHostToDevice);
-    cu_eval_curcuit <<<1, ai->nr_brain_parts, 0, ai->stream>>>
-        (cu_brainpart_value, ai->cu_brain, ai->nr_ports,
-         board->cu_board, ai->board_size);
+    cu_eval_curcuit << <ai->nr_ports / 32 + 128 / 4, ai->nr_brain_parts, 0, ai->stream>>>
+            (cu_brainpart_value, ai->cu_brain, ai->nr_ports,
+            (int*) board->cu_board, ai->board_size);
 
     //wait for all the brain parts to finish
     if (cudaDeviceSynchronize())
